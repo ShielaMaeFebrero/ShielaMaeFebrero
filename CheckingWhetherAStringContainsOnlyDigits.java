ef16906f0eddcb6ade19d88f80b822e27edcd627
ShielaMaeFebrero <100554395+ShielaMaeFebrero@users.noreply.github.com>
2022-02-28 17:32:36 +0800

    Activity 1

---
+++ CheckingWhetherAStringContainsOnlyDigits.java
@@ -0,0 +1,35 @@
+package checkingwhetherastringcontainsonlydigits.java;
+
+import java.util.Scanner;
+
+public class CheckingWhetherAStringContainsOnlyDigits {
+
+  public static void main(String[] args) {
+  
+  Scanner cs=new Scanner(System.in);
+  
+       String str;
+       int count=0,in;
+       System.out.println("Enter your String:");
+       
+       str=cs.nextLine();
+       
+       for(in=0;in<str.length();in++)
+       {
+       if(str.charAt(in) >= '0' && str.charAt(in) <= '9')
+       {
+        count++;
+             }
+                }
+       if(count==str.length())
+                  
+       System.out.print("String contains only digits.");
+                 
+       else
+                  
+       System.out.print("String does not contains only digits.");
+                  
+       cs.close();
+        
+      }                
+  }
